f0acb2a6c409cf3d1ce5ae0b5d833570dba322c5
Cindy Penders <cindy.penders@hotmail.com>
2015-03-23 21:49:50 +0100

    alle config files juist gezet

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 /*aangepast CP 23/3/2015*/
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database','session','form_validation');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url', 'form');
+$autoload['helper'] = array('url', 'form', 'html');
 
 
 /*
---
+++ application/controllers/mytodo.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Penders
+ * Date: 3/23/2015
+ * Time: 9:43 PM
+ */
